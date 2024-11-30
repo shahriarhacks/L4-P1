e1c261e41bbf81b0773dc1bd2b71af44916e38db
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-12-01 02:23:11 +0600

    ✨ remove explicit type declaration for createStudent method in user controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,10 +1,9 @@
-import { RequestHandler } from "express";
 import { UserService } from "./user.service";
 import responder from "../../utils/responder";
 import { IStudent } from "../student/student.interface";
 import asyncHandler from "../../utils/asyncHandler";
 
-const createStudent: RequestHandler = asyncHandler(async (req, res) => {
+const createStudent = asyncHandler(async (req, res) => {
    const { password, ...student } = req.body;
    const result = await UserService.createStudent(password, student);
 
