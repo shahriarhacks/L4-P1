b560fa484f838622544e3cfd021dde091ed3eb92
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-12-01 10:24:43 +0600

    ✨ remove query and params validation from requestValidator middleware

--- src/app/middleware/requestValidator.ts
+++ src/app/middleware/requestValidator.ts
@@ -7,8 +7,6 @@ const requestValidator =
       try {
          await schema.parseAsync({
             body: req.body,
-            query: req.query,
-            params: req.params,
          });
          next();
       } catch (error) {
