ff4440b8d869d5452881629f25d9463741846352
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-12-04 18:36:56 +0600

    ✨ implement createAcademicSemester function in AcademicSemesterController with success response

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,5 +1,17 @@
 import asyncHandler from "../../utils/asyncHandler";
+import responder from "../../utils/responder";
+import { AcademicSemesterService } from "./academicSemester.service";
 
-const createAcademicSemester = asyncHandler(async (req, res) => {});
+const createAcademicSemester = asyncHandler(async (req, res) => {
+   const result = await AcademicSemesterService.createAcademicSemesterIntoDB(
+      req.body,
+   );
+   responder(res, {
+      statuscode: 201,
+      success: true,
+      message: "Academic Semester created successfully",
+      data: result,
+   });
+});
 
 export const AcademicSemesterController = { createAcademicSemester };
