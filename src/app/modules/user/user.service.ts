370b157adb0c8ef2678d39be4b6a54008e9814cb
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-12-10 05:38:20 +0600

    ✨ implement student ID generation and validation for admission semester in user service

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,22 +1,31 @@
 import { environment } from "../../../config/environment";
+import { AcademicSemester } from "../academicSemester/academicSemester.model";
 import { IStudent } from "../student/student.interface";
 import { Student } from "../student/student.model";
 import { IUser } from "./user.interface";
 import { User } from "./user.model";
+import { generateStudentId } from "./user.utils";
 
-const createStudentIntoDB = async (password: string, student: IStudent) => {
+const createStudentIntoDB = async (password: string, payload: IStudent) => {
    const userData: Partial<IUser> = {};
 
-   userData.uid = "20300100002";
+   const admissionSemester = await AcademicSemester.findOne({
+      _id: payload.admissionSemester,
+   });
+   if (!admissionSemester) {
+      throw new Error("Invalid Admission semester!");
+   }
+
+   userData.uid = await generateStudentId(admissionSemester);
    userData.password = password || (environment.student_def_pass as string);
    userData.role = "student";
 
    const user = await User.create(userData);
    if (Object.keys(user).length) {
-      student.user = user._id;
-      student.uid = user.uid;
+      payload.user = user._id;
+      payload.uid = user.uid;
 
-      const createdStudent = await Student.create(student);
+      const createdStudent = await Student.create(payload);
       return createdStudent;
    }
    return undefined;
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,3 +1,33 @@
 import { IAcademicSemester } from "../academicSemester/academicSemester.interface";
+import { User } from "./user.model";
 
-const generateStudentId = async (payload: IAcademicSemester) => {};
+const findLastStudentId = async () => {
+   // Find the last student id in the database
+   const lastStudent = await User.findOne(
+      { role: "student" },
+      { uid: 1, _id: 0 },
+   )
+      .sort({ createdAt: -1 })
+      .lean();
+
+   return lastStudent?.uid ? lastStudent.uid : undefined;
+};
+
+export const generateStudentId = async (payload: IAcademicSemester) => {
+   let currentId = (0).toString();
+   const lastStudentId = await findLastStudentId();
+
+   const lastStudentYear = lastStudentId?.substring(0, 4);
+   const lastStudentCode = lastStudentId?.substring(4, 6);
+
+   if (
+      lastStudentId &&
+      lastStudentYear === payload.year &&
+      lastStudentCode === payload.code
+   ) {
+      currentId = lastStudentId.substring(6);
+   }
+   const ongoingStudentId = (Number(currentId) + 1).toString().padStart(4, "0");
+   const studentID = `${payload.year}${payload.code}${ongoingStudentId}`;
+   return studentID;
+};
