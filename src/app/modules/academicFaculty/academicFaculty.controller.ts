e16a38dd522d7ebae24132d5a6acd70da5f30048
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-12-11 17:59:48 +0600

    ✨ implement CRUD operations for academic faculty with validation and response handling

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -7,13 +7,51 @@ const createAcademicFaculty = asyncHandler(async (req, res) => {
       req.body,
    );
    responder(res, {
-      statuscode: 201,
+      statusCode: 201,
       success: true,
       message: "Academic faculty created successfully",
       data: result,
    });
 });
 
+const getSingleAcademicFaculty = asyncHandler(async (req, res) => {
+   const result = await AcademicFacultyService.getSingleFacultyFromDB(
+      req.params.id,
+   );
+   responder(res, {
+      statusCode: 200,
+      success: true,
+      message: "Academic faculty fetched successfully",
+      data: result,
+   });
+});
+
+const getAllAcademicFaculty = asyncHandler(async (_req, res) => {
+   const result = await AcademicFacultyService.getAllFacultyFromDB();
+   responder(res, {
+      statusCode: 200,
+      success: true,
+      message: "Academic faculty fetched successfully",
+      data: result,
+   });
+});
+
+const updateAcademicFaculty = asyncHandler(async (req, res) => {
+   const result = await AcademicFacultyService.updateFacultyIntoDB(
+      req.params.id,
+      req.body,
+   );
+   responder(res, {
+      statusCode: 201,
+      success: true,
+      message: "Academic faculty updated successfully",
+      data: result,
+   });
+});
+
 export const AcademicFacultyController = {
    createAcademicFaculty,
+   getSingleAcademicFaculty,
+   getAllAcademicFaculty,
+   updateAcademicFaculty,
 };
